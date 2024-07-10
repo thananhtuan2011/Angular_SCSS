95d89982bec30804746f0d273e5574ef688b6d80
Anh Tuan <thananhtuan2011@gmail.com>
2024-07-10 21:02:50 +0700

    update pipe

--- Angular_scss/src/app/app.module.ts
+++ Angular_scss/src/app/app.module.ts
@@ -5,19 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BudgetComponent } from './budget/budget/budget.component';
-
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+registerLocaleData(en);
+const Ant_Design = [
+  NzIconModule,
+  NzButtonModule
+]
 @NgModule({
   declarations: [
     AppComponent,
     BudgetComponent
   ],
   imports: [
+    Ant_Design,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
